ebdd905d413fbee19490a26881e4b1c0608ec4ab
rafaelfaria1323 <60626141+rafaelfaria1323@users.noreply.github.com>
2021-08-18 20:38:37 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyrBcj5Dqeh5yb3V0FIYHS0Wcbosd9Lwc",
-  authDomain: "chat-fire-7d63f.firebaseapp.com",
-  databaseURL: "https://chat-fire-7d63f.firebaseio.com",
-  projectId: "chat-fire-7d63f",
-  storageBucket: "chat-fire-7d63f.appspot.com",
-  messagingSenderId: "691038644982",
-  appId: "1:691038644982:web:ed1d49b873ba5fac5e9ad7",
+  //your config
 };
 
 // Initialize Firebase
